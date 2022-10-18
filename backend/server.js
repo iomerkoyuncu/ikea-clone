3a958096ba21e2b41ebf7a088854a039fc269eea
İsmet Ömer Koyuncu <ismetkync737@gmail.com>
2022-10-18 12:46:30 +0300

    register-login

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-console.log("hi");
+const express = require("express")
+const colors = require("colors")
+const dotenv = require("dotenv").config()
+
+const connectDB = require("./config/db")
+
+const errorHandler = require("./middlewares/errorMiddleware")
+
+const PORT = process.env.PORT || 3000
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+	res.status(200).json({ message: "Welcome to the ikea-clone API" })
+})
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"))
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
+})
